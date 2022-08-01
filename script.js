b308b0777e57be19c22b5903f3283a5ca7d8a9ed
ew411 <ewela9940@gmail.com>
2022-08-01 22:05:53 +0200

    js button more recipe

---
+++ script.js
@@ -0,0 +1,15 @@
+const recipes = document.getElementById('load_recipes');
+const button = document.getElementById('more');
+
+function load_more() {
+    if (recipes.style.display === 'flex') {
+        recipes.style.display = 'none';
+        button.textContent = "Load More"
+    } else {
+        recipes.style.display = 'flex';
+        recipes.style.opacity = '1';
+        button.textContent = "Close"
+    }
+}
+
+button.addEventListener('click', load_more);
